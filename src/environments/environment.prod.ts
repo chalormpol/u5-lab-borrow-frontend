79362745a005d9828c2625d46bf84398fdbc0a5e
unknown <chalormpol9899@gmail.com>
2026-01-22 12:52:43 +0700

    Fix Https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBase: 'u5-lab-borrow-api-production.up.railway.app',
+  apiBase: 'https://u5-lab-borrow-api-production.up.railway.app',
   //apiBase: 'http://localhost:5000/api',
 };
