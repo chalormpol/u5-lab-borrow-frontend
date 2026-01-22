05a2c3590a2d127a4bb9164df1aa14fc97e1a48c
unknown <chalormpol9899@gmail.com>
2026-01-22 12:34:20 +0700

    fix bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiBase:
     'u5-lab-borrow-api.railway.internalu5-lab-borrow-api-production.up.railway.app',
-  //apiBase: 'http://localhost:5000/apisadasdasdsadsadsadsadsadsad',
+  //apiBase: 'http://localhost:5000/api',
 };
